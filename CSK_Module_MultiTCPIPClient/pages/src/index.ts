ec237fc18649613b3772e40b4f0704b36e3100cc
Roy Gollub <roy.gollub@sick.de>
2024-11-28 12:25:53 +0100

    - Removing navigation bar entries

--- CSK_Module_MultiTCPIPClient/pages/src/index.ts
+++ CSK_Module_MultiTCPIPClient/pages/src/index.ts
@@ -3,6 +3,14 @@ document.addEventListener('sopasjs-ready', () => {
   page_1.classList.remove('fa-file');
   page_1.classList.add('fa-exchange');
 
+  const page_FirstLabel = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul > li:nth-child(2)');
+  const page_App = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul > li:nth-child(4)');
+  const page_Setup = document.querySelector('div.sopasjs-ui-navbar-wrapper > div > ul > li:nth-child(5) > a');
+
+  page_FirstLabel.remove();
+  page_App.remove();
+  page_Setup.remove();
+
   setTimeout(() => {
     document.title = 'CSK_Module_MultiTCPIPClient'
   }, 500);
